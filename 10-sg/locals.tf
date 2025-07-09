5b500dcaa47241a13aa59569ea7354a247b645b6
Yashasvi <yashasvi.chepa@gmail.com>
2025-07-09 14:42:18 +0530

    ansible

--- 10-sg/locals.tf
+++ 10-sg/locals.tf
@@ -1,3 +1,13 @@
 # locals {
 #     vpc_id = data.aws_ssm_parameter.vpc_id.value
 # }
+
+locals {
+  vpc_id = "vpc-xxxxxxxx"              # dummy VPC ID
+  public_subnet_ids = ["subnet-1234"]  # dummy subnet
+  common_tags = {
+    Project = var.project
+    Environment = var.environment
+    Terraform = "true"
+  }
+}
